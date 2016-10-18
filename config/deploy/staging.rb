020db53be731834d7eb86c5f3c3d6d1d4211d28e
nino <nino@opoloo.de>
2016-10-18 15:52:45 +0200

    added staging deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,10 +1,10 @@
-server 'serverurl', user: 'username', roles: %w{app db web}
+server 'vwp15223.webpack.hosteurope.de', user: 'ftp1133206', roles: %w{app db web}
 
-set :deploy_to, '/var/www/project-staging'
-set :branch, 'staging'
+set :deploy_to, '/is/htdocs/wp1133206_8C104XX0RW/www/kaffeekommune'
+set :branch, 'master'
 
 set :ssh_options, {
-  keys: %w(~/.ssh/id_rsa),
-  forward_agent: true,
-  auth_methods: %w(publickey)
+keys: %w(~/.ssh/id_rsa),
+forward_agent: true,
+auth_methods: %w(publickey)
 }
